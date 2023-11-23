23a6402e0674af8550a55ea69ec6857af1232c4a
CHRISKINUNGI <chrskinungi@gmail.com>
2023-11-24 00:59:07 +0300

    edit password

--- scheduled_tweets/config/routes.rb
+++ scheduled_tweets/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get "/about", to: "about#index"
 
+  get "password", to:"passwords#edit", as: 'edit_password'
+  patch "password", to:"passwords#update", as: 'password'
+
   get "/sign_up", to: "registrations#signup"
   post "sign_up", to: "registrations#create"
  
