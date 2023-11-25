0b8f7d73e72f9eb30bd26416f98ba872521a94a5
CHRISKINUNGI <chrskinungi@gmail.com>
2023-11-25 23:22:39 +0300

    update

--- hell0_world/config/routes.rb
+++ hell0_world/config/routes.rb
@@ -1,10 +1,7 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'hello#index'
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-end
+<% if @user.errors.any? %>
+            <div class="alert alert-danger">
+                <% @user.errors.full_messages.each do |message| %>
+                     <div><%= message %></div>
+                <% end %>
+            </div>
+        <% end %>
